01d4b1b3a18ea66d1c5e83e5f72161ce53bcb16e
curtis-wils0n <curtis.wils0n@protonmail.com>
2022-11-25 14:51:22 -0700

    Include basic API server fetching

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './styles/App.css';
 
 function App() {
+
+  const[books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const bookURL = '/api/books';
+    Promise.all([
+      axios.get(bookURL)
+    ]).then((all) => {
+      setBooks(all[0].data);
+    })
+  }, []);
+
+  const bookList = [];
+  for (const book of Object.keys(books)) {
+    if (books[book].name !== null) {
+      bookList.push(books[book].name);
+    }
+  }
+
   return (
     <div className="App">
-      <h1>Pager</h1>
+      <li>{bookList}</li>
     </div>
   );
 }
