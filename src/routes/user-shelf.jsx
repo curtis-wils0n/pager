47f0c944be9e625e63c510848286d44c2e1aab07
EVillett <emily.villett@gmail.com>
2022-11-27 12:37:51 -0800

    added search bar

--- src/routes/user-shelf.jsx
+++ src/routes/user-shelf.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import StarRating from "../components/StarRating.js";
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
 export default function Shelf() {
 
@@ -16,8 +18,21 @@ export default function Shelf() {
   }, []);
 
   return (
+    <div>
     <ul>
       {Object.keys(books).map((book) => <li>{books[book].title} ({books[book].year}) by {books[book].author_name}, published by {books[book].publisher_name} ({books[book].location})</li>)}
     </ul>
+            <Button variant="contained">Add book</Button>
+            <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="outlined-basic" label="Search" variant="outlined" />
+    </Box>
+    </div>
   );
 }
