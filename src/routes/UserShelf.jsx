587e6afc3751dfcd6d98b4b9e612a299c3c00c3e
curtis-wils0n <curtis.wils0n@protonmail.com>
2022-11-28 12:31:14 -0700

    Rename components, render all shelf items individually

---
+++ src/routes/UserShelf.jsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import UserShelfItem from "./UserShelfItem";
+import axios from "axios";
+
+export default function Shelf() {
+  
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const bookURL = '/api/books';
+    Promise.all([
+      axios.get(bookURL)
+    ]).then((all) => {
+      setBooks(all[0].data);
+    });
+  }, []);
+
+  const renderedUserShelf = Object.keys(books).map((book) => {
+    const indvBook = books[book];
+    return <UserShelfItem
+      key={indvBook.id}
+      title={indvBook.title}
+      book_cover_art_url={indvBook.cover_art_url}
+      author={indvBook.author_name}
+      year={indvBook.year}
+    />
+  }
+
+  )
+
+  return (
+    <ul>
+      {renderedUserShelf}
+    </ul>
+  );
+}
