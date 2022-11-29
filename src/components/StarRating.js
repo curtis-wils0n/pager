1b06e2cc317f545ea33b7fbf2ee4ab4526cbbb6a
EVillett <emily.villett@gmail.com>
2022-11-29 10:26:00 -0800

    added files for more components

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -1,27 +1,12 @@
-import React, { useState } from "react";
+import * as React from 'react';
+import Rating from '@mui/material/Rating';
+import Stack from '@mui/material/Stack';
 
-const StarRating = () => {
-  const [rating, setRating] = useState(0);
-  const [hover, setHover] = useState(0);
+export default function StarRating() {
   return (
-    <div className="star-rating">
-      {[...Array(5)].map((star, index) => {
-        index += 1;
-        return (
-          <button
-            type="button"
-            key={index}
-            className={index <= (hover || rating) ? "on" : "off"}
-            onClick={() => setRating(index)}
-            onMouseEnter={() => setHover(index)}
-            onMouseLeave={() => setHover(rating)}
-          >
-            <span className="star">&#9733;</span>
-          </button>
-        );
-      })}
-    </div>
+    <Stack spacing={1}>
+      <Rating name="half-rating" defaultValue={2.5} precision={0.5} />
+      <Rating name="half-rating-read" defaultValue={2.5} precision={0.5} readOnly />
+    </Stack>
   );
-};
-
-export default StarRating;
+}
