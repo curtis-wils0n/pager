c5cab30fec6f6b439f8244b1d0fd24d39d20a382
EVillett <emily.villett@gmail.com>
2022-11-29 14:00:02 -0800

    fixed styling for recommend buttons

--- src/components/NotRecommended.js
+++ src/components/NotRecommended.js
@@ -2,12 +2,13 @@ import * as React from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import ThumbDownAltRoundedIcon from '@mui/icons-material/ThumbDownAltRounded';
+import '../styles/App.css';
 
 export default function NotRecommended() {
   return (
     <Stack direction="row" spacing={2}>
-      <Button variant="outlined" color="error">
-        <ThumbDownAltRoundedIcon /> Not Recommended
+      <Button variant="contained" color="error">
+        <ThumbDownAltRoundedIcon /> This book trash!
       </Button>
     </Stack>
   );
--- src/routes/user-reviews.jsx
+++ src/routes/user-reviews.jsx
@@ -1,14 +1,9 @@
-import StarRating from "../components/StarRating.js";
-import Recommended from "../components/Recommended";
-import NotRecommended from "../components/NotRecommended";
+
 
 export default function Reviews() {
   return (
     <div>
     <p>This is the page where we put our reviews!</p>
-    <NotRecommended />
-    <Recommended />
-    <StarRating />
     </div>
   );
 }
