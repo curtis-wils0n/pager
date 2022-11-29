be8f77445919b41dfd817b93b84afd8e8a757320
EVillett <emily.villett@gmail.com>
2022-11-29 14:54:38 -0800

    created bookData hook to import into form

---
+++ src/hooks/useBookData.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function useBookData(initial) {
+
+
+  const [genre, setGenre] = React.useState();
+
+  const handleChange = (event) => {
+    setGenre(event.target.value);
+  }
+  // Replace with dynamic call to DB
+  const genres = [
+    'Action',
+    'Science Fiction',
+    'Fantasy',
+    'Romance',
+    'Manga',
+    'Non-Fiction'
+  ]
+
+
+ return { genre, handleChange, genres }
+}
