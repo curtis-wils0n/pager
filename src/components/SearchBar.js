4e51b2c9f9f3b5040dad80a48ad9fce5b311d70f
EVillett <emily.villett@gmail.com>
2022-11-29 10:19:50 -0800

    added new search bar to work with array api

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,25 +1,41 @@
 import React from 'react';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
+import parse from 'autosuggest-highlight/parse';
+import match from 'autosuggest-highlight/match';
 
 export default function SearchBar(props) {
 
   return (
     <Autocomplete
-      freeSolo
-      id="free-solo-2-demo"
-      disableClearable
-      options={props.bookData.map((option) => option.title)}
+      id="highlights-demo"
+      sx={{ width: 300 }}
+      options={props.bookData}
+      getOptionLabel={(option) => option.title}
       renderInput={(params) => (
-        <TextField
-          {...params}
-          label="Search input"
-          InputProps={{
-            ...params.InputProps,
-            type: 'search',
-          }}
-        />
+        <TextField {...params} label="Highlights" margin="normal" />
       )}
+      renderOption={(props, option, { inputValue }) => {
+        const matches = match(option.title, inputValue, { insideWords: true });
+        const parts = parse(option.title, matches);
+
+        return (
+          <li {...props}>
+            <div>
+              {parts.map((part, index) => (
+                <span
+                  key={index}
+                  style={{
+                    fontWeight: part.highlight ? 700 : 400,
+                  }}
+                >
+                  {part.text}
+                </span>
+              ))}
+            </div>
+          </li>
+        );
+      }}
     />
-);
+  );
 }
