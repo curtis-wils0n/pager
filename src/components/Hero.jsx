e88ad8f59603211b27fd9eb0c5fbdadc5f7c69b5
curtis-wils0n <curtis.wils0n@protonmail.com>
2022-12-02 16:03:49 -0700

    Create simple splash page

---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Hero(props) {
+  return (
+    <>
+      <div className="bg">
+        <h1 className="welcome">Welcome to pager</h1>
+        <h2 className="subtext">The social sharing app for modern readers</h2>
+      </div>
+    </>
+  )
+}
--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,5 +1,7 @@
+import Hero from "../components/Hero";
+
 export default function Home() {
   return (
-    <p>This is the homepage!</p>
+    <Hero/>
   );  
 }
