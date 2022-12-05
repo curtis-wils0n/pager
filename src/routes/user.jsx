80b3bce8211d7dab3215846e2448165e6fdb714f
EVillett <emily.villett@gmail.com>
2022-12-04 20:20:00 -0800

    added a profile card on the My Profile page

--- src/routes/user.jsx
+++ src/routes/user.jsx
@@ -1,5 +1,13 @@
+import '../styles/UserProfile.css';
+import UserAvatar from '../components/UserAvatar';
+
 export default function User() {
+
+
   return (
-    <p>This is the page where we display the user!</p>
+    <div className="profile-pic">
+    <UserAvatar />
+    </div>
+
   );
 }
