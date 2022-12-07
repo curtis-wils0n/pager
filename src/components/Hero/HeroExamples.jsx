6a5f8b2442f6b1a503279cbce35f69dbab7b3a1f
curtis-wils0n <curtis.wils0n@protonmail.com>
2022-12-06 21:24:21 -0700

    Make covers larger

--- src/components/Hero/HeroExamples.jsx
+++ src/components/Hero/HeroExamples.jsx
@@ -11,29 +11,29 @@ export default function HeroExamples(props) {
           className="book-cover"
           src="https://github.com/curtis-wils0n/pager-api/blob/main/src/db/assetts/Dune_cover.jpeg?raw=true"
           alt="Dune cover"
-          width={150}
-          height={225}
+          width={200}
+          height={300}
         />
         <img 
           className="book-cover"
           src="https://github.com/curtis-wils0n/pager-api/blob/main/src/db/assetts/battle-angel-cover.jpeg?raw=true"
           alt="Alita Battle Angel cover"
-          width={150}
-          height={225}
+          width={200}
+          height={300}
         />
         <img 
           className="book-cover"
           src="https://github.com/curtis-wils0n/pager-api/blob/main/src/db/assetts/nineteen-eighty-four-1984-cover.jpeg?raw=true" 
           alt="1984 cover" 
-          width={150}
-          height={225} 
+          width={200}
+          height={300} 
         />
         <img
           className="book-cover"
           src="https://github.com/curtis-wils0n/pager-api/blob/main/src/db/assetts/house-of-suns-cover.jpeg?raw=true"
           alt="To Kill a Mocking Bird cover"
-          width={150}
-          height={225}
+          width={200}
+          height={300}
         />
       </div>
     </div>
